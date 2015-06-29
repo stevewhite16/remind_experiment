d0abb36b3f360c865b4575c11a8c7cd1263e7f2b
Steve White <stevewhite16@gmail.com>
2015-06-29 08:32:24 -0400

    Created basic file names
    .gitignore, observational study file and power analysis file

---
+++ observational_study.R
@@ -0,0 +1,10 @@
+# Power analysis for remind.com Unit Test evaluation
+
+# Load data from the Excel files using the xlsx package
+
+
+# run linear models using a ton of dummies variables
+
+
+
+# analyze the size of the effect of getting reminders - how big? how precisely estimated?
---
+++ power_analysis.R
@@ -0,0 +1,5 @@
+# Power Analysis for remind.com UT Project
+
+# do a power analysis, how big would the effect need to be for us to detect it on 50-50 randomization?
+# calibrate with variance of UT, variance of section effects, do sections even matter? (F-test on that)
+# this should probably be done in Stata to make life easier for me
