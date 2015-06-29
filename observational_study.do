6e5f00e4c3547375a1c1b4638b15dca52c5787fb
Steve White <stevewhite16@gmail.com>
2015-06-29 17:35:22 -0400

    Updated power_analysis.R
    Fixed the observational_study to output some data to use for
    bootstrapped power analysis using R. The power_analysis in R was
    significantly updated and the .gitignore file now ignores anything
    ending in .R*

--- observational_study.do
+++ observational_study.do
@@ -19,5 +19,12 @@ areg UT remind i.semester, absorb(sl_code)
 areg UT remind i.sl_code i.semester, absorb(id) cluster(id)
 areg UT remind xile_final i.sl_code i.semester, absorb(id) cluster(id)
 
+areg UT remind i.sl_code i.semester, absorb(id) cluster(id)
+predict ut_twiddle, resid
+areg remind i.sl_code i.semester, absorb(id) cluster(id)
+predict remind_twiddle, resid
+
+outsheet ut_twiddle remind_twiddle UT remind sl_code semester id using "data/temp/twiddle.csv", comma replace
+
 * most of the results show a robust increase in UT, despite lots of controls
 * its hard to do power analysis because its hard to estimate all the variances
--- power_analysis.R
+++ power_analysis.R
@@ -1,5 +1,21 @@
 # Power Analysis for remind.com UT Project
 
-# do a power analysis, how big would the effect need to be for us to detect it on 50-50 randomization?
-# calibrate with variance of UT, variance of section effects, do sections even matter? (F-test on that)
-# this should probably be done in Stata to make life easier for me
+setwd("~/GitHub/remind_experiment")
+data <- read.csv("data/temp/twiddle.csv")
+
+# draw bootstrapped samples w/ 200 kids from my section and 300 from other sections
+n_r <- 200
+remind <- (data$remind == 1)
+set_of_ids <- 1:nrow(data)
+remind_index <- set_of_ids[remind]
+boot_id_r <- sample(remind_index, n_r, replace=TRUE)
+boot_sample_r <- data[boot_id_r, ]
+
+n_not <- 300
+not_remind <- (data$remind == 0)
+set_of_ids <- 1:nrow(data)
+not_remind_index <- set_of_ids[not_remind]
+boot_id_n <- sample(not_remind_index, n_r, replace=TRUE)
+boot_sample_n <- data[boot_id_n, ]
+
+boot_data <- rbind(boot_sample_r, boot_sample_n)
