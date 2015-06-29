d629f7ff4b6fc5589e302621a1bafcd3f17c6364
Steve White <stevewhite16@gmail.com>
2015-06-29 18:08:10 -0400

    Minor updates to power_analysis.R
    Minor modifications to power_analysis.R to store more output.

--- power_analysis.R
+++ power_analysis.R
@@ -22,18 +22,19 @@ remind_effect <- function (data, n_r, n_not) {
   
   boot_data <- rbind(boot_sample_r, boot_sample_n)
   
-  model <- lm(UT ~ remind + semester + sl_code + id, boot_data)
+  model <- lm(UT ~ remind + semester + id, boot_data)
   return(model$coefficients[2])
 }
 
-set.seed(1)
-reps <- 10000
+#set.seed(1)
+reps <- 500
 beta_remind <- 1:reps
 for (i in 1:reps) {
     beta_remind[i] <- remind_effect(data, 300, 300)
 }
 
-summary(beta_remind)
+CI_90 <- c(quantile(beta_remind, 0.025), quantile(beta_remind, 0.975))
+# wide CIs, even at 90% we are underpowered according to this
 
 # cannot use built-in boot function because it need indicies parameter
 # beta_remind <- boot(data=data, statistic=remind_effect, R=100, 200, 300)
